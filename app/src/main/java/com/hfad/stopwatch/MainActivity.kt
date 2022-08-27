f75c478974fd276b8d2e3ed160df1d1c0020ddf1
Khalida Aliyeva <khld.aliyeva@gmail.com>
2022-08-27 21:22:07 +0400

    Override onPause and onResume instead of onStop and onRestart to include the case where activity is partially visible

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -69,16 +69,16 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(savedInstanceState)
     }
 
-    override fun onStop() {
-        super.onStop()
+    override fun onPause() {
+        super.onPause()
         if (running) {
             saveOffset()
             stopwatch.stop()
         }
     }
 
-    override fun onRestart() {
-        super.onRestart()
+    override fun onResume() {
+        super.onResume()
         if (running) {
             setBaseTime()
             stopwatch.start()
