b027a2dd1a7be2064b0ce05f8d11692103a05825
Khalida Aliyeva <khld.aliyeva@gmail.com>
2022-08-27 19:35:13 +0400

    Add onClickListener to each button to control the stopwatch

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.hfad.stopwatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var stopwatch: Chronometer  // The stopwatch
+    private var running = false  // Is the stopwatch running?
+    private var offset: Long = 0  // The base offset for the stopwatch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById(R.id.stopwatch)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+
+        val btnPause = findViewById<Button>(R.id.btnPause)
+        btnPause.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+
+        val btnReset = findViewById<Button>(R.id.btnReset)
+        btnReset.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
     }
 }
